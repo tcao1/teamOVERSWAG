5bd0352672b3c4935a2ceff7274a865ebcc0083d
c <c>
2014-02-13 13:00:00 -0600

    Clean Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamOVERSWAG::Application.config.secret_token = '34d7e4e7e677c23e8f0ebbf8120dc76afccec4bbd43247afead9a7e42801e713dc6c67b217e8c7bf07aface0483ab50567e2ee35be6a727e87a24a28a1fcfe5c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TeamOVERSWAG::Application.config.session_store :cookie_store, key: '_teamOVERSWAG_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TeamOVERSWAG::Application.config.session_store :active_record_store
